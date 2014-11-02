6183125e731fff042ca8ff01d976c3f184533b3e
karelmikie3 <karelkling1@gmail.com>
2014-11-02 21:50:30 +0100

    improved the homing arrow a bit

--- src/main/java/HxCKDMS/XEnchants/hooks/ArrowEventHookContainer.java
+++ src/main/java/HxCKDMS/XEnchants/hooks/ArrowEventHookContainer.java
@@ -1,5 +1,6 @@
 package HxCKDMS.XEnchants.hooks;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import HxCKDMS.XEnchants.XEnchants;
@@ -11,6 +12,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
@@ -72,47 +74,38 @@ public class ArrowEventHookContainer
 	{
         if (event.entity instanceof EntityArrow){
 			EntityArrow arrow = (EntityArrow) event.entity;
-            if(isHoming)
-            {
-				if(target == null || target.velocityChanged || !target.canEntityBeSeen(arrow))
-				{
-					double posX = arrow.posX;
-					double posY = arrow.posY;
-					double posZ = arrow.posZ;
-					double size = 6 * homingAmount;
-					double d = -1D;
-					EntityLiving entityliving = null;
-					List list = arrow.worldObj.getEntitiesWithinAABB(net.minecraft.entity.EntityLiving.class, arrow.boundingBox.expand(size, size, size));
-
+            if(isHoming) {
+                if(target == null || target.velocityChanged || !target.canEntityBeSeen(arrow)) {
+                    double posX = arrow.posX;
+                    double posY = arrow.posY;
+                    double posZ = arrow.posZ;
+                    double size = 6 * homingAmount;
+                    double d = -1D;
+                    EntityLiving entityliving = null;
+                    List list = arrow.worldObj.getEntitiesWithinAABB(net.minecraft.entity.EntityLiving.class, arrow.boundingBox.expand(size, size, size));
                     for (Object aList : list) {
                         EntityLiving tempEnt = (EntityLiving) aList;
-
                         if (tempEnt == arrow.shootingEntity) {
                             continue;
                         }
-
                         double distance = tempEnt.getDistance(posX, posY, posZ);
-
                         if ((size < 0.0D || distance < size * size) && (d == -1D || distance < d) && tempEnt.canEntityBeSeen(arrow)) {
                             d = distance;
                             entityliving = tempEnt;
                         }
                     }
-
-					target = entityliving;
-				}
-
-				if(target != null)
-				{
+                    target = entityliving;
+                }
+                if(target != null ){
                     if(target instanceof EntityLiving) {
-                        // All these fancy calculations guarantee that it will hit an entity dead on
                         double dirX = target.posX - arrow.posX;
                         double dirY = target.boundingBox.minY + (double) (target.height / 2.0F) - (arrow.posY + (double) (arrow.height / 2.0F));
                         double dirZ = target.posZ - arrow.posZ;
                         arrow.setThrowableHeading(dirX, dirY, dirZ, 1.5F, 0.0F);
+                        arrow.setVelocity(dirX*10, dirY*10, dirZ*10);
                     }
-				}
-			}
+                 }
+            }
         }
 	}
 }
