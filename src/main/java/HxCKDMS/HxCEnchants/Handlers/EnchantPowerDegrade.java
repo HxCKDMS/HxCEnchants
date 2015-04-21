ef12cd7bb6d12730f1de0cc94ba2f4fce2430e0f
DrZed <kyahne12@yahoo.com>
2015-04-21 16:43:39 -0400

    I'm working on something :3

--- src/main/java/HxCKDMS/HxCEnchants/Handlers/EnchantPowerDegrade.java
+++ src/main/java/HxCKDMS/HxCEnchants/Handlers/EnchantPowerDegrade.java
@@ -1,9 +1,23 @@
 package HxCKDMS.HxCEnchants.Handlers;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingEvent;
 
 public class EnchantPowerDegrade {
     public void degrade(ItemStack stack, int Enchantment, int Amount){
 
+    }
+    @SubscribeEvent
+    public void onPlayerUpdate(LivingEvent.LivingUpdateEvent event){
+        if (event.entityLiving instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer)event.entityLiving;
+            for (int i = 0; i < 4; i++) {
+                ItemStack stack = player.inventory.armorItemInSlot(i);
+                int[] enchants = stack.getTagCompound().getIntArray("HxCEnchants");
+
+            }
+        }
     }
 }
