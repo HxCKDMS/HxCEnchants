70afa3851287743a34db96700fb626285ae20241
DrZed <kyahne12@yahoo.com>
2015-07-01 15:25:10 -0400

    Fixed NPE
    Signed-off-by: DrZed <kyahne12@yahoo.com>

--- src/main/java/HxCKDMS/HxCEnchants/lib/Reference.java
+++ src/main/java/HxCKDMS/HxCEnchants/lib/Reference.java
@@ -3,6 +3,6 @@ package HxCKDMS.HxCEnchants.lib;
 public class Reference {
     public static final String MOD_ID = "HxCEnchants";
     public static final String MOD_NAME = "HxC Enchants";
-    public static final String VERSION = "1.8-1.5.1";
+    public static final String VERSION = "1.8-1.5.2";
     public static final String DEPENDENCIES = "required-after:HxCCore@[1.5.1,)";
 }
